7f97c253ef0b46a0226608fe64e4282dbdf931b1
Nathan Sprague <spragunr@jmu.edu>
2020-11-01 19:35:30 -0500

    initial commit

---
+++ launch/aruco_demo.launch.py
@@ -0,0 +1,38 @@
+"""
+
+"""
+
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # We need to get the path to the .rviz file...
+    this_prefix = get_package_share_directory('zeta_rescue')
+    rviz_path = os.path.join(this_prefix, 'rviz', 'aruco.rviz')
+
+    return LaunchDescription([
+        Node(
+            package="jmu_turtlebot3_bringup",
+            node_executable="tb_fixer",
+            node_name="tb_fixer",
+            output="screen",
+        ),
+        Node(
+            package="rviz2",
+            node_executable="rviz2",
+            node_name="rviz2",
+            output="screen",
+            arguments=['-d', rviz_path]
+        ),
+        Node(
+            package="ros2_aruco",
+            node_executable="aruco_node",
+            output="screen",
+            parameters=[{'camera_frame': 'camera_rgb_optical_frame'}] ,
+            arguments=['-d', rviz_path]
+        )
+
+    ])
---
+++ zeta_rescue/rescue_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from zeta_rescue.')
+
+
+if __name__ == '__main__':
+    main()
