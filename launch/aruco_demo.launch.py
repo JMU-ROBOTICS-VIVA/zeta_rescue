49db79bd31076bdb06bb220594e667e8fdc02b73
Nathan Sprague <nathan.r.sprague@gmail.com>
2023-10-20 08:17:45 -0400

    small changes for humble.  tested in simulation.

--- launch/aruco_demo.launch.py
+++ launch/aruco_demo.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
             package="rviz2",
             executable="rviz2",
             name="rviz2",
-            output="screen",
+            output={'both': 'log'},
             arguments=['-d', rviz_path],
             parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
         ),
